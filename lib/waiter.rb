5f01b8b8f919a7a978ce736818bde669493a3a66
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-01 21:42:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
+  attr_accessor :years, :name
+
+  @@all = []
+
+  def initialize(name, years)
+    @years = years
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def 
+
 
 end
